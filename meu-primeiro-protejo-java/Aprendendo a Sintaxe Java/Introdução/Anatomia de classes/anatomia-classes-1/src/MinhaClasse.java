54db3a449bb636c6106250071be413df3404c656
trevisanilucas <lcstrevisani@gmail.com>
2024-05-06 13:41:41 -0300

    Fim Anatomia de classes

--- meu-primeiro-protejo-java/Aprendendo a Sintaxe Java/Introdução/Anatomia de classes/anatomia-classes-1/src/MinhaClasse.java
+++ meu-primeiro-protejo-java/Aprendendo a Sintaxe Java/Introdução/Anatomia de classes/anatomia-classes-1/src/MinhaClasse.java
@@ -1,16 +1,12 @@
 public class MinhaClasse {
     
 public static void main (String [] args) {
-    
     String primeiroNome = "Lucas";
     String segundoNome = "Trevisani";
-    
     String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
     System.out.println(nomeCompleto);
 
 }
-
-
     public static String nomeCompleto (String primeiroNome, String segundoNome) {
         return " Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 
