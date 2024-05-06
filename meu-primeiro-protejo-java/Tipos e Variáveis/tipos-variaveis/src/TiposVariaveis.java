895294bee528767c9c3d388f0d88bf3457a2bfc1
trevisanilucas <lcstrevisani@gmail.com>
2024-05-06 15:45:24 -0300

    Operadores

--- meu-primeiro-protejo-java/Tipos e Variáveis/tipos-variaveis/src/TiposVariaveis.java
+++ meu-primeiro-protejo-java/Tipos e Variáveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+    
+    int numero1 = 1;
+    int numero2 = 2;
+    int numero3 = 5;
+    int numero4 = 4;
+
+    if (numero1 < numero2 || numero3 < numero4) {
+        System.out.println("A nossa condição é verdadeira");
+    }   else {
+        System.out.println("A nossa condição é falsa");
+        }    
+    
+    
+    //boolean simNao = numero1 == numero2;
+    
+    //System.out.println("numeroUm é igual a numeroDois?" +simNao);
+
+    //simNao = numero1 != numero2;
+    
+    //System.out.println("numeroUm é igual a numeroDois?" +simNao);
+    
+
+        }
 }
+
